c80fb8d41a8a63928f5330ea6eb2c7a90ffa78db
leehojin-93 <hogenie93@gmail.com>
2021-06-04 13:00:15 +0900

    21. 6. 4. 금) 수업_static기능

---
+++ src/com/javaex/ex11/MyMath.java
@@ -0,0 +1,36 @@
+package com.javaex.ex11;
+
+public class MyMath {
+
+	// 필드
+
+
+
+	// 생성자 - default 사용
+
+
+
+	// 메소드 게터세터
+
+
+
+	// 메소드 일반 - 메소드 오버로딩
+	public static int plus(int a, int b) {		
+		int result = a + b; // 복잡한 계산식 가정		
+		System.out.println(result);
+		return result;
+	}
+
+	public static double plus(double a, double b) {
+		double result = a + b;
+		return result;
+	}
+
+	/*
+	public static void result() {
+		System.out.println(result);
+	}
+	 */
+
+
+}
---
+++ src/com/javaex/ex11/MyMathApp.java
@@ -0,0 +1,19 @@
+package com.javaex.ex11;
+
+public class MyMathApp {
+	public static void main(String[] args) {
+		
+//		MyMath myMath = new MyMath(); --> 이렇게 쓸 이유가 없다. --> static기능 활용
+		
+		MyMath.plus(100, 100);
+		
+		System.out.println(MyMath.plus(100.5, 100.2));
+		
+		System.out.println(MyMath.plus(99.5, 35));
+		
+		System.out.println(MyMath.plus(95, 35.7));
+		
+		MyMath.plus(111, 111);
+	}
+
+}
