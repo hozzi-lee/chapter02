9cda7bcd6375f240ec937af18820f05b0c371575
leehojin-93 <hogenie93@gmail.com>
2021-06-09 10:20:41 +0900

    21. 6. 9. 수) 상속(일반화) 예제풀이 수정

--- src/com/javaex/ex17/ShapeApp.java
+++ src/com/javaex/ex17/ShapeApp.java
@@ -19,7 +19,11 @@ public class ShapeApp {
 		rect01.draw();
 		System.out.println();
 		// Rectangle.class Test_Array
-		Rectangle[] rectArray = { new Rectangle("RED", "BLACK", 5, 5), new Rectangle("BLUE", "GRAY", 5, 5), new Rectangle("PINK", "HOTPINK", 5, 5) };
+		Rectangle[] rectArray = {
+				new Rectangle("RED", "BLACK", 5, 5),
+				new Rectangle("BLUE", "GRAY", 5, 5),
+				new Rectangle("PINK", "HOTPINK", 5, 5)
+		};
 		for ( int i = 0; i < rectArray.length; i++ ) {
 			rectArray[i].draw();
 			System.out.println(rectArray[i]); // .toString() 생략가능
@@ -35,6 +39,17 @@ public class ShapeApp {
 		System.out.println(circ01.toString());
 		circ01.draw();
 		System.out.println();
+		// Circle.class Test_Array
+		Circle[] circArray = {
+				new Circle("RED", "BLACK", 5),
+				new Circle("BLUE", "RED", 10),
+				new Circle("SKYBLUE", "HOTPINK", 15)
+		};
+		for ( int i = 0; i < circArray.length; i++ ) {
+			circArray[i].draw();
+			System.out.println(circArray[i]); // .toString() 생략가능
+			System.out.println();
+		}
 		System.out.println();
 		System.out.println();
 
@@ -44,6 +59,17 @@ public class ShapeApp {
 		Triangle triag01 = new Triangle("블루", "핑크", 7, 7);
 		System.out.println(triag01.toString());
 		triag01.draw();
+		System.out.println();
+		// Triangle.class Test_Array
+		Triangle[] triagArray = {
+				new Triangle("BLUE", "PINK", 5, 5),
+				new Triangle("SKYBLUE", "HOTPINK", 10, 10),
+				new Triangle("YELLOW", "GREEN", 15, 15)
+		};
+		for ( int i = 0; i < triagArray.length; i++ ) {
+			triagArray[i].draw();
+			System.out.println(triagArray[i]); // .toString() 생략가능
+		}
 
 	}
 
